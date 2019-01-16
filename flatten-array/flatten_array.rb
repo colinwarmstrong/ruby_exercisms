c051f5500830d2d4f170a103efb13d14741cf07c
Colin Armstrong <colinwarmstrong@gmail.com>
2019-01-16 00:51:26 -0700

    Complete first four Ruby exercism exercises

---
+++ flatten-array/flatten_array.rb
@@ -0,0 +1,5 @@
+module FlattenArray
+  def self.flatten(initial_array)
+    initial_array.flatten.compact
+  end
+end
---
+++ gigasecond/gigasecond.rb
@@ -0,0 +1,5 @@
+module Gigasecond
+  def self.from(initial_time)
+    initial_time + 10**9
+  end
+end
